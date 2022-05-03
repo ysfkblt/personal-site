7233175a3325f857c892b58dc61a975a2433bbe9
Yusuf Akbulut <80647654+ysfkblt@users.noreply.github.com>
2022-05-03 18:27:56 +0300

    Responsive Design Updated

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import avatar from './assets/avatar.jpg';
 function App() {
 	return (
 		<div className='grid h-screen place-items-center bg-[#090909] select-none text-neutral-500'>
-			<div className='inline-grid h-screen items-end'>
-				<p className='leading-none text-[18vw] xl:text-[8vw] ml-[10vw] xl:ml-0 '>
+			<div className='inline-grid h-screen xl:items-end'>
+				<p className='leading-none text-[18vw] xl:text-[8vw] ml-[10vw] xl:ml-0 mt-[13vh] xl:mt-0 '>
 					Hey! I’m <span className='text-amber-300'>Yusuf</span>
 					<br />
 					Front-end Developer <br /> & UI Designer.
 				</p>
-				<div className='inline-flex mb-[3vh]  xl:mb-[5vh] justify-between mx-[10vw] xl:mx-0 '>
+				<div className='inline-flex mb-[5vh]   justify-between mx-[10vw] xl:mx-0 '>
 					<div className='inline-flex self-end'>
 						<p className='text-[.5rem] xl:text-xs text-gray-400 tracking-widest'>
 							MAIL ME
@@ -23,17 +23,18 @@ function App() {
 							</div>
 						</p>
 					</div>
-
-					<a
-						target='_blank'
-						rel='noopener noreferrer'
-						href='https://github.com/ysfkblt'
-					>
-						<img
-							className='h-[14vw] xl:h-[8vw] border-black border rounded-full object-cover'
-							src={avatar}
-						/>
-					</a>
+					<div className='inline-flex self-end'>
+						<a
+							target='_blank'
+							rel='noopener noreferrer'
+							href='https://github.com/ysfkblt'
+						>
+							<img
+								className='h-[14vw] xl:h-[8vw] border-black border rounded-full object-cover'
+								src={avatar}
+							/>
+						</a>
+					</div>
 				</div>
 			</div>
 		</div>
