8a7ee7b4824b273ec32c6a0c28913b6709b62b27
Yusuf Akbulut <80647654+ysfkblt@users.noreply.github.com>
2022-08-08 19:12:31 +0300

    Design Updates

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 
 function App() {
 	return (
-		<React.Fragment>
+		<>
 			<div className='grid h-screen md:h-max xl:h-screen content-evenly justify-center bg-neutral-300 dark:bg-[#090909] dark:text-neutral-500  text-zinc-800 select-none  transition-all -mb-2'>
 				<div className='leading-none text-[18vw] xl:text-[8vw] ml-[10vw] xl:ml-0 mt-[10vh] xl:mt-[18vh] '>
 					Hey! I’m{' '}
@@ -78,10 +78,10 @@ function App() {
 						</a>
 					</div>
 				</div>
-				<ChevronDownIcon className='hidden xl:block absolute  text-blue-600 dark:text-amber-200 bottom-4 left-1/2 arrowanimation h-[2vh] xl:h-[1.5vw] opacity-70' />
+				{/* <ChevronDownIcon className='hidden xl:block absolute  text-blue-600 dark:text-amber-200 bottom-4 left-1/2 arrowanimation h-[2vh] xl:h-[1.5vw] opacity-70' /> */}
 			</div>
 			{/* // CASE // */}
-			<div className='flex flex-col items-start bg-neutral-300 dark:bg-[#090909] dark:text-neutral-500  text-zinc-800 select-none  transition-all leading-none pl-[11vw] pt-[2vh] md:pt-[8vh] pb-6 xl:pt-24 xl:pb-8 xl:pl-[12vw] text-[11vw] xl:text-[5vw] space-y-4  '>
+			{/* <div className='flex flex-col items-start bg-neutral-300 dark:bg-[#090909] dark:text-neutral-500  text-zinc-800 select-none  transition-all leading-none pl-[11vw] pt-[2vh] md:pt-[8vh] pb-6 xl:pt-24 xl:pb-8 xl:pl-[12vw] text-[11vw] xl:text-[5vw] space-y-4  '>
 				<p className='font-bold pb-2 xl:pb-4 '>Projects</p>
 				<p>
 					Netflix
@@ -154,8 +154,8 @@ function App() {
 						/>
 					</a>
 				</p>
-			</div>
-		</React.Fragment>
+			</div> */}
+		</>
 	);
 }
 
